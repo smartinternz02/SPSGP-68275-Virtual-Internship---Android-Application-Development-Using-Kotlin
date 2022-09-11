3bd2379a1173544005ab8436226d8d62422a89c8
Ss Hemanth <sshemanthbollepalli@gmail.com>
2022-09-11 08:17:54 +0530

    Initial Commit

---
+++ Forage-App/app/src/main/java/com/example/forage/data/ForageDatabase.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.forage.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.forage.model.Forageable
+
+/**
+ * Room database to persist data for the Forage app.
+ * This database stores a [Forageable] entity
+ */
+@Database(entities = [Forageable::class], version = 1, exportSchema = false)
+abstract class ForageDatabase : RoomDatabase() {
+    abstract fun forageableDao(): ForageableDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ForageDatabase? = null
+
+        fun getDatabase(context: Context) = INSTANCE ?: synchronized(this) {
+            Room.databaseBuilder(
+                context.applicationContext,
+                ForageDatabase::class.java,
+                "forageable_database"
+            ).build().apply {
+                INSTANCE = this
+            }
+        }
+    }
+}
---
+++ Forage-App/app/src/main/java/com/example/forage/ui/viewmodel/ForageableViewModel.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.forage.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.forage.data.ForageableDao
+import com.example.forage.model.Forageable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+/**
+ * Shared [ViewModel] to provide data to the [ForageableListFragment], [ForageableDetailFragment],
+ * and [AddForageableFragment] and allow for interaction the the [ForageableDao]
+ */
+
+class ForageableViewModel(
+    private val forageableDao: ForageableDao
+) : ViewModel() {
+    val forageables = forageableDao.getForagebles().asLiveData()
+
+    fun getForageable(id: Long) = forageableDao.getForageable(id).asLiveData()
+
+    fun addForageable(
+        name: String,
+        address: String,
+        inSeason: Boolean,
+        notes: String
+    ) {
+        val forageable = Forageable(
+            name = name,
+            address = address,
+            inSeason = inSeason,
+            notes = notes
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            forageableDao.insert(forageable)
+        }
+    }
+
+    fun updateForageable(
+        id: Long,
+        name: String,
+        address: String,
+        inSeason: Boolean,
+        notes: String
+    ) {
+        val forageable = Forageable(
+            id = id,
+            name = name,
+            address = address,
+            inSeason = inSeason,
+            notes = notes
+        )
+        viewModelScope.launch(Dispatchers.IO) {
+            forageableDao.update(forageable)
+        }
+    }
+
+    fun deleteForageable(forageable: Forageable) {
+        viewModelScope.launch(Dispatchers.IO) {
+            forageableDao.delete(forageable)
+        }
+    }
+
+    fun isValidEntry(name: String, address: String): Boolean {
+        return name.isNotBlank() && address.isNotBlank()
+    }
+}
+
+class ForageableViewModelFactory(private val forageableDao: ForageableDao) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ForageableViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ForageableViewModel(forageableDao) as T
+        }
+        throw IllegalArgumentException("Unexpected class: $modelClass")
+    }
+}
